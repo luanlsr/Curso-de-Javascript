282a3b64aa65e001758cf9fbacc6731b6dde22a7
Luan da Silva Ramalho <luanfswd@gmail.com>
2020-11-30 02:32:07 -0300

    appendChild

---
+++ js/script3.js
@@ -0,0 +1,6 @@
+var p = document.createElement("img")
+p.src = "/javascriptC.png"
+
+document.getElementById("conteudo").appendChild(p)
+
+p.style.height = "300px"
--- js/scripts2.js
+++ js/scripts2.js
@@ -1,7 +1,7 @@
 
 var foto = document.getElementById("foto")
 
-foto.src = "javascriptC.png"
+foto.src = "/javascriptC.png"
 foto.style.border = "3px solid red"
 foto.style.borderRadius = "50%"
 foto.style.height = "300px"
