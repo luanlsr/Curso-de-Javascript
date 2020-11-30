15b50857dabcd712df8400b233f2251953522bd9
Luan da Silva Ramalho <luanfswd@gmail.com>
2020-11-30 02:05:31 -0300

    getElementById

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var paragrafo = document.getElementById("p1")
+paragrafo.innerHTML = "Novo Conteúdo"
+
+paragrafo.style.color = "red"
+paragrafo.style.backgroundColor = "#333"
+paragrafo.style.height = "400px"
